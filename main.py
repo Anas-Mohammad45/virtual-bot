a0be319aab44398f775c2111c6a41c672483f3a9
Anas-Mohammad45 <mohammad.anas43@outlook.com>
2025-03-10 00:52:40 +0530

    Added Project

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import os
-
 import eel
 
-eel.init("www")
+from engine.features import *
+from engine.command import *
 
-os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
-eel.start('index.html', mode=None, host='localhost', block=True)
+def start():
+    
+     eel.init("www")
+
+
+     playAssistantSound()
+
+     os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+
+     eel.start('index.html', mode=None, host='localhost', block=True)
+
